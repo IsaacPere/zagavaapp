9c4795dfdc60701c2c5e834c81541c7c89626750
Isaac Pere <perendegwa@gmail.com>
2026-06-16 13:44:13 +0300

    Updated the zagva autoapply functionality

--- auto/zagava_autoapply.hpp
+++ auto/zagava_autoapply.hpp
@@ -8,6 +8,10 @@
 
 namespace starting_scarpping_operations {
     inline std::string zagava_base_url_details;
+    inline std::string portal_url_details;
+    inline std::string zagava_username_details;
+    inline std::string zagava_password_details;
+
     inline void establishing_web_scarping_information() {
         std::cout << "Enter the base website url : ";
         std::cin >> zagava_base_url_details;
@@ -17,6 +21,33 @@ namespace starting_scarpping_operations {
             curl_easy_setopt(zagava_scarp_details, CURLOPT_URL, zagava_base_url_details.c_str());
             zagava_scarpping_result = curl_easy_perform(zagava_scarp_details);
             curl_easy_cleanup(zagava_scarp_details);
+        }
+    }
+    inline void establishing_portal_url() {
+        std::cout << "Enter the portal url : ";
+        std::cin >> portal_url_details;
+        CURL *portal_url_details_curl = curl_easy_init();
+        if(portal_url_details_curl) {
+            CURLcode portal_url_details_result;
+            curl_easy_setopt(portal_url_details_curl, CURLOPT_URL, portal_url_details.c_str());
+            portal_url_details_result = curl_easy_perform(portal_url_details_curl);
+            curl_easy_cleanup(portal_url_details_curl);
+        }
+    }
+    inline void establishing_zagava_credentials() {
+        std::cout << "Enter the zagava username : ";
+        std::cin >> zagava_username_details;
+        std::cout << "Enter the zagava password : ";
+        std::cin >> zagava_password_details;
+        CURL *zagava_credentials_curl = curl_easy_init();
+        if(zagava_credentials_curl) {
+            CURLcode zagava_credentials_result;
+            curl_easy_setopt(zagava_credentials_curl, CURLOPT_URL, zagava_base_url_details.c_str());
+            curl_easy_setopt(zagava_credentials_curl, CURLOPT_USERNAME, zagava_username_details.c_str());
+            curl_easy_setopt(zagava_credentials_curl, CURLOPT_PASSWORD, zagava_password_details.c_str());
+            curl_easy_setopt(zagava_credentials_curl, CURLOPT_PORT, 1L);
+            zagava_credentials_result = curl_easy_perform(zagava_credentials_curl);
+            curl_easy_cleanup(zagava_credentials_curl);
         }
     }
 }
